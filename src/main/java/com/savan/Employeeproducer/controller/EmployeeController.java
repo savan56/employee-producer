e797abe61d3cb61d1c9bcf55c205c1429163c6a8
savan.inexture <savan.inexture@gmail.com>
2021-01-29 11:41:09 +0530

    initial commit

---
+++ src/main/java/com/savan/Employeeproducer/controller/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.savan.Employeeproducer.controller;
+
+import com.savan.Employeeproducer.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employee")
+    public Employee firstPage(){
+        Employee emp = new Employee();
+        emp.setName("emp1");
+        emp.setDesignation("manager");
+        emp.setEmpId("1");
+        emp.setSalary(3000);
+        return emp;
+    }
+
+}
